80f3e12ed11ffe5dcf57a3c73ad897afdf7f0114
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-14 10:12:24 -0500

    Java practice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
 
         askTheQuestion();
         doKitchenLogic();
-
+        doUpstairsLogic();
     }
 
 
@@ -29,6 +29,47 @@ public class App {
 
 
         if (input.equals("kitchen")) {
+            System.out.println("There is a long counter top with dirty dishes everywhere.  Off to one side\n" +
+                    "there is, as you'd expect, a refrigerator. You may open the \"refrigerator\"\n" +
+                    "or look in a \"cabinet\".");
+            input = scan.nextLine().toLowerCase();
+
+            if (input.equals("refrigerator")) {
+                System.out.println("Inside the refrigerator you see food and stuff.  It looks pretty nasty.\n" +
+                        "Would you like to eat some of the food? (\"yes\" or \"no\")");
+
+                input = scan.nextLine().toLowerCase();
+
+                if (input.equals("no")) {
+                    System.out.println("You die of starvation... eventually.");
+                }
+                if (input.equals("yes")) {
+                    System.out.println("You don't die of starvation... eventually.");
+                }
+            }
+
+            if (input.equals("cabinet")) {
+                System.out.println("Inside the cabinet you see a fully charged laptop.  Looks like someone installed intelliJ.\n" +
+                        "Would Would you like to code? (\"yes\" or \"no\")");
+
+                input = scan.nextLine().toLowerCase();
+
+                if (input.equals("no")) {
+                    System.out.println("You should make tome to practice Java.");
+                }
+                if (input.equals("yes")) {
+                    System.out.println("I think this is the right decision so you stay sharp with code");
+                }
+            }
+        }
+    }
+
+    public static void doUpstairsLogic() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine().toLowerCase();
+
+
+        if (input.equals("upstairs")) {
             System.out.println("There is a long counter top with dirty dishes everywhere.  Off to one side\n" +
                     "there is, as you'd expect, a refrigerator. You may open the \"refrigerator\"\n" +
                     "or look in a \"cabinet\".");
