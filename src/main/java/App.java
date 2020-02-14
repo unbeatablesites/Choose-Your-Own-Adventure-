792d67bd686aa836207820d950023ece6916d90c
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-13 23:44:38 -0500

    Trying something new

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,19 @@ public class App {
 
 
     public static void askTheQuestion(){
+        System.out.println("WELCOME TO MITCHELL'S TINY ADVENTURE!\n");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("WELCOME TO MITCHELL'S TINY ADVENTURE!\n");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Scanner scan = new Scanner(System.in);
         scan.nextLine();
