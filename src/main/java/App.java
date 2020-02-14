3d3982ee2e62c764e0766fbd4d1072e3442d0bde
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-14 10:10:27 -0500

    Java practice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -47,6 +47,20 @@ public class App {
                     System.out.println("You don't die of starvation... eventually.");
                 }
             }
+
+            if (input.equals("cabinet")) {
+                System.out.println("Inside the cabinet you see a fully charged laptop.  Looks like someone installed intelliJ.\n" +
+                        "Would Would you like to code? (\"yes\" or \"no\")");
+
+                input = scan.nextLine().toLowerCase();
+
+                if (input.equals("no")) {
+                    System.out.println("You should make tome to practice Java.");
+                }
+                if (input.equals("yes")) {
+                    System.out.println("I think this is the right decision so you stay sharp with code");
+                }
+            }
         }
     }
 }
