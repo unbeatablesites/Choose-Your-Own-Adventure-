e5a89834a81269df631ecedd39ec49d74762822b
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-14 00:02:36 -0500

    Trying something new with thread

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -42,8 +42,9 @@ public class App {
 
                 input = scan.nextLine().toLowerCase();
 
-
-
+                if(input.equals("no")){
+                    System.out.println("Well, then I guess you'll never know what was in there.  Thanks for playing,\n" +
+                            "I'm tired of making nested if statements.");
             }
 
         }
