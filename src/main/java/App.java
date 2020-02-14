999fc4f4ffc1f322a111f339c828f806bc00a71c
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-14 10:04:37 -0500

    Java practice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
 
         askTheQuestion();
-        doLogic();
+        doKitchenLogic();
 
     }
 
@@ -23,23 +23,18 @@ public class App {
                 "\"kitchen\"?");
     }
 
-    public static void doLogic() {
+    public static void doKitchenLogic() {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine().toLowerCase();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        if (input.equals("upstairs")) {
-            System.out.println("Upstairs you see a hallway.  At the end of the hallway is the master\n" +
-                    "\"bedroom\".  There is also a \"bathroom\" off the hallway.  Where would you like\n" +
-                    "to go?");
+        if (input.equals("kitchen")) {
+            System.out.println("There is a long counter top with dirty dishes everywhere.  Off to one side\n" +
+                    "there is, as you'd expect, a refrigerator. You may open the \"refrigerator\"\n" +
+                    "or look in a \"cabinet\".");
             input = scan.nextLine().toLowerCase();
 
-            if (input.equals("bedroom")) {
+            if (input.equals("refrigerator")) {
                 System.out.println("You are in a plush bedroom, with expensive-looking hardwood furniture.  The\n" +
                         "bed is unmade.  In the back of the room, the closet door is ajar.  Would you\n" +
                         "like to open the door? (\"yes\" or \"no\")");
