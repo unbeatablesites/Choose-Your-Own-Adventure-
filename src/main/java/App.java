b6bb7ac130cd0c652af97991f350010e50b0da3a
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-13 23:39:49 -0500

    Starter code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
     }
+
+
+    public static void askTheQuestion(){
+
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine();
+
+
+    }
+
 }
