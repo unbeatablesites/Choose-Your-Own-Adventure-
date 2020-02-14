d2798160fe7cd9a85dd85bafdf20162d64c84ba9
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-13 23:50:37 -0500

    Trying something new with thread

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,10 +18,12 @@ public class App {
         System.out.println("You are in a creepy house!  Would you like to go \"upstairs\" or into the\n" +
                 "\"kitchen\"?");
 
-
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine().toLowerCase();
 
+    }
+
+    public static void doLogic(){
 
     }
 
